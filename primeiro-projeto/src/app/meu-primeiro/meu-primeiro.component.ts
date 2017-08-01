4445ece88cdd54d38e836e477d611d1d081b5182
Nicholas Giudice <nicholas.giudice@outlook.com>
2017-07-31 22:17:36 -0300

    Primeiro Exemplo

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import{ Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+    <p> Meu Primeiro Componente com Angular 2</p>
+    `
+})
+export class MeuPrimeiroComponent {
+
+}
